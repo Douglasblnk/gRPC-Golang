7dbec92095956a3dd2e2b27f5a8f9146fd0c7490
Douglas Blank <douglas.blank@multiplier.com.br>
2022-04-14 11:24:50 -0300

    feat(customer): implementa extração do body e validação

---
+++ customer/api/utils/get_body.go
@@ -0,0 +1,17 @@
+package utils
+
+import (
+	"github.com/gofiber/fiber/v2"
+)
+
+func GetBody[T any](model T, c *fiber.Ctx) []*ErrorResponse {
+	c.BodyParser(model)
+
+	err := ValidateStruct(model)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
