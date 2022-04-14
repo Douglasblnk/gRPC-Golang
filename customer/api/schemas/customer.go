ca88f1e178b38aeab4066df0d3e87bd1bace724d
Douglas Blank <douglas.blank@multiplier.com.br>
2022-04-14 11:24:31 -0300

    feat(customer): implementa esquema para extração do payload

---
+++ customer/api/schemas/customer.go
@@ -0,0 +1,11 @@
+package schemas
+
+type CreateCustomer struct {
+	Name  string `json:"name" validate:"required"`
+	Email string `json:"email" validate:"required,email"`
+}
+
+type UpdateCustomer struct {
+	Name  *string `json:"name" validate:"required"`
+	Email *string `json:"email" validate:"required,email"`
+}
