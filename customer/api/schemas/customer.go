de3b068bc84b88c11e451eee266350b0f16dbcd9
Douglas Blank <douglas.blank@multiplier.com.br>
2022-04-18 15:22:06 -0300

    feat(customer): remove validação de campos na atualização

--- customer/api/schemas/customer.go
+++ customer/api/schemas/customer.go
@@ -6,6 +6,6 @@ type CreateCustomer struct {
 }
 
 type UpdateCustomer struct {
-	Name  *string `json:"name" validate:"required"`
-	Email *string `json:"email" validate:"required,email"`
+	Name  *string `json:"name"`
+	Email *string `json:"email" validate:"email"`
 }
