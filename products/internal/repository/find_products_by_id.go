6b501c99df1f04a148cf9f6fdc8ca506d1bc9ef7
Douglas Blank <douglas.blank@multiplier.com.br>
2022-05-05 17:03:26 -0300

    feat: remove print desnecessário

--- products/internal/repository/find_products_by_id.go
+++ products/internal/repository/find_products_by_id.go
@@ -1,7 +1,6 @@
 package repository
 
 import (
-	"fmt"
 	"microservice-poc/products/internal/database"
 	"microservice-poc/products/internal/exceptions"
 	"microservice-poc/products/internal/models"
@@ -11,7 +10,6 @@ func FindProductByID(id string) (*models.Product, error) {
 	product := &models.Product{}
 
 	if err := database.DB.Where("id = ?", id).First(product).Error; err != nil {
-		fmt.Println("err", err)
 		return nil, exceptions.ErrProductNotFound
 	}
 
